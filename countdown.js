a2302efe2cc8287fcc7e564ed5a2096aa741246a
Spleefies <cutecatty32@gmail.com>
2025-12-29 22:00:01 +0800

    26

--- countdown.js
+++ countdown.js
@@ -1,16 +1,16 @@
 checkbox = document.querySelector("input")
 function countdown(date, id) {
-    let examDate = new Date(date).getTime();
+    let targetDate = new Date(date).getTime()
     
     let x = setInterval(() => {
-        let now = new Date().getTime();
-        let timeLeft = examDate - now;
+        let now = new Date().getTime()
+        let timeLeft = targetDate - now
         
-        let days = Math.floor(timeLeft / 86400000);
-        let hours = Math.floor((timeLeft % 86400000) / 3600000);
-        let minutes = Math.floor((timeLeft % 3600000) / 60000);
-        let seconds = Math.floor((timeLeft % 60000) / 1000);
-        let milliseconds = (timeLeft % 1000);
+        let days = Math.floor(timeLeft / 86400000)
+        let hours = Math.floor((timeLeft % 86400000) / 3600000)
+        let minutes = Math.floor((timeLeft % 3600000) / 60000)
+        let seconds = Math.floor((timeLeft % 60000) / 1000)
+        let milliseconds = (timeLeft % 1000)
         
         document.getElementById(id).innerHTML =
             days + "d " +
@@ -22,10 +22,9 @@ function countdown(date, id) {
         }
 
         if (timeLeft < 0) {
-            clearInterval(x);
+            clearInterval(x)
             document.getElementById(id).parentElement.style.display = 'none'
         }
-    }, 1);
-};
-countdown("Nov 10, 2025 08:00:00", "ProgressReport");
-countdown("Nov 10, 2025 13:00:00", "SIL");
+    }, 15)
+}
+countdown("Jan 1, 2026 00:00:00", "2026");

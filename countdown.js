1063c8331ed4b5c77c72aaa3ad036be8f7215ea5
Spleefies <cutecatty32@gmail.com>
2026-01-27 20:37:45 +0800

    bio

--- countdown.js
+++ countdown.js
@@ -29,7 +29,7 @@ function countdown(date, id) {
         }
     }, 16.67)
 }
-countdown("Jan 28, 2026 09:15:00", "BL2131-enzyme-quiz")
+countdown("Jan 30, 2026 10:15:00", "BL2131-enzyme-quiz")
 countdown("Feb 24, 2026 10:15:00", "CH2531-t1")
 // countdown("Mar 5, 2026 08:00:00", "PC2131-ct1")
 countdown("Mar 10, 2026 10:00:00", "CH2531-compo1")

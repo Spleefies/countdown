041efc26d3a9e8756e6cbf3eddc2e58f18d76453
Spleefies <cutecatty32@gmail.com>
2026-02-13 10:03:45 +0800

    common test

--- countdown.js
+++ countdown.js
@@ -29,7 +29,6 @@ function countdown(date, id) {
         }
     }, 16.67)
 }
-countdown("Feb 6, 2026 11:35:00", "MA2133-gt1")
 countdown("Feb 18, 2026 10:15:00", "GE2133-class-quiz")
 countdown("Feb 24, 2026 10:15:00", "CH2531-t1")
 countdown("Mar 10, 2026 10:00:00", "CH2531-compo1")
@@ -37,3 +36,12 @@ countdown("Apr 7, 2026 10:00:00", "CH2531-compo2")
 countdown("Apr 21, 2026 10:00:00", "CH2531-email")
 countdown("May 5, 2026 10:15:00", "CH2531-t2")
 countdown("May 8, 2026 10:15:00", "BL2131-practical")
+//c
+countdown("Mar 3, 2026 13:00:00", "MA2133-ct")
+countdown("Mar 3, 2026 15:00:00", "EL2131-ct")
+countdown("Mar 5, 2026 13:00:00", "PC2131-ct")
+countdown("Mar 5, 2026 15:00:00", "BL2131-ct")
+countdown("Apr 27, 2026 08:30:00", "EL2131-ct2")
+countdown("Apr 27, 2026 10:30:00", "CM2131-ct")
+countdown("Apr 30, 2026 08:30:00", "MA2133-ct2")
+countdown("Apr 30, 2026 10:30:00", "BL2131-ct2")

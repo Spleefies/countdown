665d39b5a0ddaae64e81593bbc51d4ba179bd757
Spleefies <cutecatty32@gmail.com>
2026-02-24 19:24:52 +0800

    geo\ngra\nphy

--- countdown.js
+++ countdown.js
@@ -29,8 +29,8 @@ function countdown(date, id) {
         }
     }, 16.67)
 }
-countdown("Feb 18, 2026 10:15:00", "GE2133-class-quiz")
-countdown("Feb 24, 2026 10:15:00", "CH2531-t1")
+countdown("Feb 25, 2026 10:15:00", "GE2133-class-quiz")
+countdown("Feb 27 2026 13:00:00", "MA2133-gt2")
 countdown("Mar 10, 2026 10:00:00", "CH2531-compo1")
 countdown("Apr 7, 2026 10:00:00", "CH2531-compo2")
 countdown("Apr 21, 2026 10:00:00", "CH2531-email")
